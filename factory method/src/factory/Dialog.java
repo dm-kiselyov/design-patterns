8795fe85e3c4e9fccff5102533e52c3d4f710ce7
dm.kiselev <dm.kiselev@tinkoffinsurance.ru>
2022-03-15 13:54:15 +0300

    Creational Patterns

---
+++ factory method/src/factory/Dialog.java
@@ -0,0 +1,15 @@
+package factory;
+
+import buttons.Button;
+
+public abstract class Dialog {
+
+    public void renderWindow() {
+        // ... остальной код диалога ...
+
+        Button okButton = createButton();
+        okButton.render();
+    }
+
+    public abstract Button createButton();
+}
